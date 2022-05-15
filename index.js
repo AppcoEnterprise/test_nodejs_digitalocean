a6e73f62628ed9ee51833016e604583bae580a8c
Chhann Chikay <appco.enterprise.2021@gmail.com>
2022-05-15 20:23:27 +0700

    xxx

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// Initialize DB
+require('./initDB')();
+
 app.get("/", (req, res) => {
   return res.status(200).json({
     status: 200,

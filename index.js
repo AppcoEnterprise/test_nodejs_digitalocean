7837a8c20ea0813786e31285db0097eda22eaa40
Chhann Chikay <appco.enterprise.2021@gmail.com>
2022-05-15 21:00:04 +0700

    xxx

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Initialize DB
 require('./initDB')();
+
+const ProductRoute = require('./Routes/Product.route');
+app.use('/products', ProductRoute);
+
 
 app.get("/", (req, res) => {
   return res.status(200).json({
